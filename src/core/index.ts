e564b7dbcdba01ac13eb447eda4a5134215d5101
Gabriel Paulucci <mrvulcandragonex@gmail.com>
2020-10-15 15:48:43 -0300

    Hendler de comandos

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,20 +1,56 @@
 import { Client } from "discord.js";
+import glob from "glob";
+import { executeCommand } from "./events/message";
+import { Command, ICommands } from "./models/commands";
 
 class BotDiaHalloween {
     client: Client
-    private _token!: string
+    private _token: string
+    private _commands: ICommands
 
     constructor() {
         this.client = new Client()
+        this._token = ''
+        this._commands = {}
     }
 
     set token(value: string) {
         this._token = value
     }
 
+    registerCommands (): void {
+        glob('./src/core/commands/**/*.ts', {
+            absolute: true
+        }, (error, files) => {
+            if (error) {
+                console.error(error)
+                return
+            }
+
+            console.log('Loading commands')
+            let i = 0
+            for (const file of files) {
+                const command = require(file).default
+
+                if (command instanceof Command) {
+                    this._commands[command.name] = command
+                    for (const alia of command.alias) {
+                        this._commands[alia] = command
+                    }
+                    i++
+                }
+            }
+            console.log(i + ' commands load')
+        })
+    }
+
     start(): void {
         this.client.on('ready', () => {
             console.log('Bot iniciado')
+        })
+
+        this.client.on('message', message => {
+            executeCommand(message, this._commands, this.client)
         })
 
         this.client.login(this._token)
---
+++ src/core/models/commands.ts
@@ -0,0 +1,39 @@
+import { IContext } from "./contexts";
+
+export interface ICommandInfo {
+    description: string
+    module: string
+    usage: string[]
+}
+
+export interface ICommand {
+    name: string
+    alias: string[]
+    info: ICommandInfo
+
+    execCommand(context: IContext): void
+    visible(): boolean
+}
+
+export interface ICommands {
+    [keyof: string]: ICommand
+}
+
+export abstract class Command implements ICommand {
+    abstract name: string;
+    abstract alias: string[];
+    info: ICommandInfo;
+
+    constructor () {
+        this.info = {
+            description: 'Sem descrição informada',
+            module: 'Default',
+            usage: []
+        }
+    }
+
+    abstract execCommand(context: IContext): void
+    visible (): boolean {
+        return true
+    }
+}
